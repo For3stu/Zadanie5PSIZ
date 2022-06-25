0aded4bc7590b3c542347fa6c480a38855dc0ef0
For3stu <70603167+For3stu@users.noreply.github.com>
2022-06-25 19:20:12 +0200

    Infomracje na temat zdjecia

--- Zadanie5PSIZ/Zadanie5PSIZ.cpp
+++ Zadanie5PSIZ/Zadanie5PSIZ.cpp
@@ -2,9 +2,14 @@
 
 #include <iostream>
 #include <fstream>
+#include <vector>
+#include "bmp.h"
 
 using namespace std;
 
+vector<char> buffer;
+PBITMAPFILEHEADER file_header;
+PBITMAPINFOHEADER info_header;
 
 
 void fill() {
@@ -15,12 +20,41 @@ void fill() {
         streampos length = file.tellg();
         file.seekg(0, std::ios::beg);
 
+        buffer.resize(length);
+        file.read(&buffer[0], length);
+
+        file_header = (PBITMAPFILEHEADER)(&buffer[0]);
+        info_header = (PBITMAPINFOHEADER)(&buffer[0] + sizeof(BITMAPFILEHEADER));
     }
 }
 
 int main() {
 
     fill();
+    cout << "buffer: ";
+    cout << buffer[0] << buffer[1] << endl;
+    cout << "Rozmiar : ";
+    cout << file_header->bfSize << endl;
+    cout << "Szerokosc: ";
+    cout << info_header->biWidth << endl;
+    cout << "Wysokosc: ";
+    cout << info_header->biHeight << endl;
+    cout << "Liczba platkow: ";
+    cout << info_header->biPlanes << endl;
+    cout << "Liczba bitow na piksel: ";
+    cout << info_header->biBitCount << endl;
+    cout << "Kompresja: ";
+    cout << info_header->biCompression << endl;
+    cout << "Rozmiar samego rysunku: ";
+    cout << info_header->biSizeImage << endl;
+    cout << "Rozdzielczosc pozioma: ";
+    cout << info_header->biXPelsPerMeter << endl;
+    cout << "Rozdzielczosc pionowa: ";
+    cout << info_header->biYPelsPerMeter << endl;
+    cout << "Liczba kolorow w palecie: ";
+    cout << info_header->biClrUsed << endl;
+    cout << "Wazne kolory w palecie: ";
+    cout << info_header->biClrImportant << endl;
 
     return 0;
 }
